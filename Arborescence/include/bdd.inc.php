b8a268cfe2cb680c6defeb0f7e3fddbdfc572e12
Raphaël D <daniel.rapahel@hotmail.fr>
2022-09-22 11:04:56 +0200

    Connexion PDO
    j'ai mis la connexion PDO (uniquement)

--- Arborescence/include/bdd.inc.php
+++ Arborescence/include/bdd.inc.php
@@ -2,7 +2,19 @@
 //Mettre les include des classe içi
 
 //Mettre la connexion PDO
-
+$user="root";
+    $mdp="";
+    $serveur="localhost"; // Ou information fournie par l'hébergeur
+    $bd="centre_equestre";
+    $dns="mysql:host=$serveur;dbname=$bd";
+    try{
+       $idcon=new PDO($dns, $user, $mdp);
+       $idcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+       // autres commandes (voir plus loin)
+    }
+    catch (PDOException $e){
+      echo"Erreur de connexion à la base de donnée : " .$e->getMessage();
+    }
 
 
 ?>
