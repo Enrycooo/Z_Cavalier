1a4d4c613432f341056f039627a1c9c7313d2eac
Raphaël D <daniel.rapahel@hotmail.fr>
2022-09-22 17:28:26 +0200

    ajout de la classe cours

--- Arborescence/include/class/Cours.inc.php
+++ Arborescence/include/class/Cours.inc.php
@@ -1,3 +1,33 @@
 <?php
-
+class Cours{
+    private $lib_cours;
+    private $date;
+    private $duree;
+    
+    public function __construct($lib,$date,$d){
+        $this -> lib_cours = $lib;
+        $this -> date = $date;
+        $this -> duree = $d;
+    }
+    public function get_lib_cours (){
+        Return $this -> lib_cours;
+    }
+    public function set_lib_cours ($n){
+        $this -> lib_cours = $lib;
+    }
+    
+    public function get_date () {
+        Return $this -> date;
+    }
+    public function set_date ($date) {
+        $this -> date = $date;
+    }
+    public function get_duree () {
+        Return $this -> duree;
+    }
+    public function set_duree ($d) {
+        $this -> duree = $d;
+    }
+}
+?>
 ?>
