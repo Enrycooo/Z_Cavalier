90d8acb5b9391b033adecfd2183a9debf2607fc1
Raphaël D <daniel.rapahel@hotmail.fr>
2022-09-22 11:45:04 +0200

    copier coller d'exemple

--- Arborescence/Cavalier/Cavalier_trait.php
+++ Arborescence/Cavalier/Cavalier_trait.php
@@ -1,2 +1,64 @@
 <?php
 //Cette page est dédiée aux traitements
+
+/*
+ Exemple de traitement
+<?php 
+
+require '../../lib/includes/defines.inc.php';
+
+
+if(isset($_POST["create"])){
+    if(empty($_POST["casier_name"])){
+        ?>
+            <script>
+                window.location.replace("http://localhost/uvlight/atelier1/casier/index.php?nav=read%22);
+            </script>
+        <?php
+    }
+    $req = $oCasier->db_create($_POST["casier_name"]);
+    if($req){
+        ?>
+            <script>
+                window.location.replace("http://localhost/uvlight/atelier1/casier/index.php?nav=read%22);
+            </script>
+        <?php
+    }
+}elseif(isset($_POST["update"])){
+    $req = $oCasier->db_update_lib($_POST["casier_id"], $_POST["casier_name"]);
+    if($req){
+        ?>
+            <script>
+                window.location.replace("http://localhost/uvlight/atelier1/casier/index.php?nav=read%22);
+            </script>
+        <?php
+    }
+}elseif(isset($_POST["delete"])){
+    $req = $oCasier->db_soft_delete_one($_POST["casier_id"]);
+    if($req){
+        ?>
+            <script>
+                window.location.replace("http://localhost/uvlight/atelier1/casier/index.php?nav=read%22);
+            </script>
+        <?php
+    }
+}elseif(isset($_POST["multi_delete"])){
+    $array = $_POST["array"];
+    $finalarray = json_decode($array, true);
+    $ids = [];
+    foreach ($finalarray as $key => $value) {
+        $id = (int) $value;
+        array_push($ids, $id);
+    }
+
+    $res = $oCasier->db_soft_delete_multi($ids);
+    if($res){
+        $response = "ok";
+    }else{
+        $response = "pb";
+    }
+    echo json_encode($response);
+}*/
+
+
+?>
