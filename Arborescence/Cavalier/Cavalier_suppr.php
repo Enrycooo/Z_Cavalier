f261a8300c094ed3379a88dc518ff1fb882278f1
Raphaël D <daniel.rapahel@hotmail.fr>
2022-09-26 17:15:21 +0200

    BO suppression cavalier

--- Arborescence/Cavalier/Cavalier_suppr.php
+++ Arborescence/Cavalier/Cavalier_suppr.php
@@ -1,6 +1,7 @@
 <?php
+include_once('../include/defines.inc.php');
 
-if ($_POST['delete'] != ""){
+if ($_POST['id'] != ""){
     $id = $_POST['id'];
     
     $requete = "DELETE FROM personne
@@ -10,6 +11,13 @@ if ($_POST['delete'] != ""){
     ?>
         <script>
             alert("Cela a fonctionné")
+            window.location.replace("http://localhost/Z_Cavalier/Arborescence/Cavalier/Cavalier_Affiche.php");
+        </script>
+    <?php
+    }else {
+    ?>
+        <script>
+            alert("Un problème a été rencontré")
             window.location.replace("http://localhost/Z_Cavalier/Arborescence/Cavalier/Cavalier_Affiche.php");
         </script>
     <?php
