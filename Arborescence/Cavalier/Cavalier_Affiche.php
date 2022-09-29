035f05beda83ec37ee4683437990fcae280cbc7e
Raphaël D <daniel.rapahel@hotmail.fr>
2022-09-29 17:41:41 +0200

    Version 2.0 du BO cavalier (utilisation d'uvlight de l'année dernière)

--- Arborescence/Cavalier/Cavalier_Affiche.php
+++ Arborescence/Cavalier/Cavalier_Affiche.php
@@ -1,26 +1,203 @@
 <?php
-include_once('../include/defines.inc.php');
+include('../include/defines.inc.php');
 $sql = ("SELECT * FROM personne");
+/*
 ?>
-<html>
-    <head>
-        <title>Back Office Cavalier</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../static/css/bootstrap.min.css">
-    </head>
-    <body>
-        <form action="Cavalier_trait.php" method="post">
-            <center>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.2/datatables.min.css"/>
+    <script>
+        function autocomplet() {
+        var min_length = 3; // nombre de caractère avant lancement recherche
+        var keyword = $('#departement').val();  // departement fait référence au champ de recherche puis lancement de la recherche grace ajax_refres
+        if (keyword.length >= min_length) {
+            $.ajax({
+                url: 'ajax_refresh.php',
+                type: 'POST',
+                data: {keyword:keyword},
+                success:function(data){
+                    $('#departement_list').show();
+                    $('#departement_list').html(data);
+                }
+            });
+        } else {
+            $('#departement_list').hide();
+        }
+    }
+
+    // Lors du choix dans la liste
+    function set_item(item) {
+        // change input value
+        $('#departement').val(item);
+        // hide proposition list
+        $('#departement_list').hide();
+    }
+    </script>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+    <!-- jQuery Library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
+
+    
+    <title>Cavalier</title>
+</head>
+<body>
+
+<?php
+    if(!isset($_GET["nav"]) || $_GET["nav"] === "read"){
+
+  ?>
+  <div class="container pt-5">
+    <a class="btn btn-success mb-4" href="index.php?nav=create">Créer une nouvelle commune</a>
+
+    <table id="table">
+        <thead>
+            <th>Id Cavalier</th>
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>Date de naissance</th>
+            <th>E-mail</th>
+            <th>Telephone</th>
+            <th>Galop</th>
+            <th>Numéro licence</th>
+        </thead>
+    </table>
+  </div>
+    
+
+    <script> //initialisation datatable
+        $(document).ready(function(){
+            $('#table').DataTable({
+                'processing': true,
+                'serverSide': true,
+                'serverMethod': 'post',
+                'ajax': {
+                    'url':'ajaxfile.php'
+                },
+                'columns': [
+                    { data: 'id' },
+                    { data: 'nom' },
+                    { data: 'prenom' },
+                    { data: 'DNA' },
+                    { data: 'mail' },
+                    { data: 'telephone' },
+                    { data: 'galop' }
+                    { data: 'numerolicence' }
+                ],
+                deferRender:    true,
+                scrollCollapse: true,
+                scroller:       true
+            });
+        });
+    </script>
+
+    <?php
+    }
+    elseif($_GET['nav'] === "create"){
+        $cavalier = $oCavalier->db_get_all();
+        ?>
+            <h1>Créer une ville</h1>
+
+            <form action="Cavalier_trait.php" method="post">
                 <input placeholder="Nom" type="text" name="nom">
                 <input placeholder="Prenom" type="text" name="prenom">
-                <input placeholder="Date de naissance" type="text" name="dna">
-                <input placeholder="e-mail" type="text" name="mail">
-                <input placeholder="telephone" type="text" name="telephone">
-                <input placeholder="galop" type="text" name="galop">
-                <input placeholder="numero de licence" type="text" name="numerolicence">
-                <button name="create" type="submit id="submit">Enregistrer</button>
-            </center>
+                <input placeholder="Date de naissance" type="text" name="DNA">
+                <input placeholder="Adresse mail" type="text" name="mail">
+                <input placeholder="Numéro de téléphone" type="text" name="telephone">
+                <input placeholder="Galop" type="text" name="galop">
+                <input placeholder="Numéro de licence" type="text" name="numerolicence">
+                <label for="">Cavalier</label>
+                <button name="create" type="submit">Enregistrer</button>
+            </form>
+
+        <?php
+    }
+    elseif($_GET["nav"] === "update"){
+        $data = $oCavalier->db_get_by_id($_GET["id"]);
+        ?>
+            <form action="Cavalier_trait.php" method="post">
+                <input type="hidden" name="id" value="<?php echo $data["id"] ?>">
+                <input placeholder="Nom" type="text" name="nom" value="<?php echo $data["nom"] ?>">
+                <input placeholder="Prenom" type="text" name="prenom" value="<?php echo $data["prenom"] ?>">
+                <input placeholder="Date de naissance" type="text" name="DNA" value="<?php echo $data["DNA"] ?>">
+                <input placeholder="Adresse mail" type="text" name="mail" value="<?php echo $data["mail"] ?>">
+                <input placeholder="Numéro de téléphone" type="text" name="telephone" value="<?php echo $data["telephone"] ?>">
+                <input placeholder="galop" type="text" name="galop" value="<?php echo $data["galop"] ?>">
+                <input placeholder="Numéro de licence" type="text" name="numerolicence" value="<?php echo $data["numerolicence"] ?>">
+                <button name="update" type="submit">Enregistrer</button>
+            </form>
+        <?php
+
+    }
+    elseif($_GET["nav"] === "delete"){
+        $data = $oCavalier->db_get_by_id($_GET["id"]);
+        ?>
+            <form action="Cavalier_trait.php" method="post">
+                <input disabled type="text" name="nom" value="<?php echo $data["nom"]; ?>">
+                <input type="hidden" name="id" value="<?php echo $_GET["id"]; ?>">
+                <button name="delete" type="submit">Supprimer</button>
+            </form>
+        <?php
+
+    }
+    
+    
+    ?>
+</body>
+</html>
+
+<?php
+*/
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../static/css/bootstrap.min.css">
+</head>
+    <body>
+        
+        <form action="Cavalier_trait.php" method="post">
+            
+                <div class="form-group container">
+                    <label>Nom :</label>
+                <input placeholder="Nom" class="form-control" style="width: 25%;" type="text" name="nom">
+                </div>
+                <div class="form-group container">
+                    <label>Prenom :</label>
+                <input placeholder="Prenom" class="form-control" style="width: 25%;" type="text" name="prenom">
+                </div>
+                <div class="form-group container">
+                    <label>Date de naissance :</label>
+                <input placeholder="Date de naissance" class="form-control" style="width: 25%;" type="text" name="dna">
+                </div>
+                <div class="form-group container">
+                    <label>Adresse mail :</label>
+                <input placeholder="e-mail" class="form-control" style="width: 25%;" type="text" name="mail">
+                </div>
+                <div class="form-group container">
+                    <label>Numéro de téléphone :</label>
+                <input placeholder="telephone" class="form-control" style="width: 25%;" type="text" name="telephone">
+                </div>
+                <div class="form-group container">
+                    <label>Galop :</label>
+                <input placeholder="galop" class="form-control" style="width: 25%;" type="text" name="galop">
+                </div>
+                <div class="form-group container">
+                    <label>Numéro de licence :</label>
+                <input placeholder="numero de licence" class="form-control" style="width: 25%;" type="text" name="numerolicence">
+                </div>
+                <div class="form-group container">
+                <button type='button' name="create" class='btn btn-primary form-control' style="width: 25%;" type="submit id="submit"> Enregistrer </button>
+                </div>
         </form>
         <div class="container">
             <div class="row">
@@ -76,3 +253,4 @@ $sql = ("SELECT * FROM personne");
         </form>
     </body>
 </html>
+*/
--- Arborescence/Cavalier/Cavalier_trait.php
+++ Arborescence/Cavalier/Cavalier_trait.php
@@ -2,6 +2,44 @@
 include_once('../include/defines.inc.php');
 //Cette page est dédiée aux traitements
 
+if(isset($_POST["create"])){
+    $sql = $conn->prepare("SELECT id FROM personne WHERE nom = :nom");
+    $sql->bindValue(':nom', $_POST["departement"]);
+    $sql->execute();
+    $departement_id = $sql->fetch(PDO::FETCH_ASSOC);
+    $departement_id = $departement_id["d_id"];
+    $req = $oCommunes->db_create($_POST["insee_code"], $_POST["zip_code"], $_POST["city_name"], $departement_id, $_POST["lat"], $_POST["lng"]);
+    if($req){
+        ?>
+            <script>
+                alert("Cela a fonctionné")
+                window.location.replace("http://localhost/uvlight/atelier1/commune/index.php?nav=read");
+            </script>
+        <?php
+    }
+}elseif(isset($_POST["update"])){
+    $req = $oCommunes->db_update_one($_POST["city_id"], $_POST["city_name"], $_POST["insee_code"], $_POST["zip_code"], $_POST["lat"], $_POST["lng"]);
+    if($req){
+        ?>
+            <script>
+                alert("Cela a fonctionné")
+                window.location.replace("http://localhost/uvlight/atelier1/commune/index.php?nav=read");
+            </script>
+        <?php
+    }
+}elseif(isset($_POST["delete"])){
+    $req = $oCommunes->db_soft_delete_one();
+    if($req){
+        ?>
+            <script>
+                alert("Cela a fonctionné")
+                window.location.replace("http://localhost/uvlight/atelier1/commune/index.php?nav=read");
+            </script>
+        <?php
+    }
+}
+
+/*
 if($_POST['nom'] != "" && $_POST['prenom'] != "" && $_POST['dna'] != "" && $_POST['mail'] != "" && $_POST['telephone'] != "" && $_POST['galop'] != "" && $_POST['numerolicence'] != ""){
     
     $nom=$_POST['nom'];
@@ -32,4 +70,6 @@ if($_POST['nom'] != "" && $_POST['prenom'] != "" && $_POST['dna'] != "" && $_POS
     <?php
     }
 }
+
+ */
 ?>
