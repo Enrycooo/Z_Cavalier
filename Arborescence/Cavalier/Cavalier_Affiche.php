37c6426ce33ab10302af4dec2ec45abe7f5f037a
Raphaël D <daniel.rapahel@hotmail.fr>
2022-10-20 15:36:39 +0200

    Suppression de ligne et fichier inutile

--- Arborescence/Cavalier/Cavalier_Affiche.php
+++ Arborescence/Cavalier/Cavalier_Affiche.php
@@ -23,7 +23,7 @@ include('../include/defines.inc.php');
     <div class="container">
         <div class="d-flex justify-content-center">
             <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modalCreate'>Création de cavalier</button>  
-            <<form action="Cavalier_search.php" method='post'></form>
+            <form action="Cavalier_search.php" method='post'></form>
             <input placeholder="Nom" type="text" name="nom">
             <input placeholder="Prenom" type="text" name="prenom">
             <button name="search" type="submit id="submit">Rechercher</button>
@@ -162,80 +162,5 @@ include('../include/defines.inc.php');
                     </div>
                 </div>
             </div>
-        <?php
-/*
-    ?>
-
-
-?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../static/css/bootstrap.min.css">
-</head>
-    <body>
-        <div class="container" style="padding:25px">
-        <input type="submit" class="btn btn-primary" value="Créer un nouveau cavalier"
-        onclick="window.location='Cavalier_Ajouter.php';" />    
-        <form action="Cavalier_search.php" method="post" style='position: absolute;top: 25px; left: 1005px;'>
-                <input placeholder="Nom" type="text" name="nom">
-                <input placeholder="Prenom" type="text" name="prenom">
-                <button name="create" type="submit" class='btn btn-primary' id="submit">Chercher</button>
-        </form>
-        </div>
-
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <table class='table table-hover'>
-                    <thead>
-                        <tr>
-                            <th style='text-align :center'>id</th>
-                            <th style='text-align :center'>Nom</th>
-                            <th style='text-align :center'>Prénom</th>
-                            <th style='text-align :center'>Date de naissance</th>
-                            <th style='text-align :center'>adresse mail</th>
-                            <th style='text-align :center'>téléphone</th>
-                            <th style='text-align :center'>galop</th>
-                            <th style='text-align :center'>Numéro de licence</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php 
-                    foreach ($conn->query($sql) as $row) { ?>
-                        <tr data-value="<?php echo $row["id_personne"] ?>">
-                            <td><center><?php echo $row["id_personne"] ?></center></td>
-                            <td><center><?php echo $row["nom"] ?></center></td>
-                            <td><center><?php echo $row["prenom"] ?></center></td>
-                            <td><center><?php echo $row["DNA"] ?></center></td>
-                            <td><center><?php echo $row["mail"] ?></center></td>
-                            <td><center><?php echo $row["telephone"] ?></center></td>
-                            <td><center><?php echo $row["galop"] ?></center></td>
-                            <td><center><?php echo $row["numerolicence"] ?></center></td>
-                            <td style='display:flex; justify-content: space-evenly;'>
-                            <form action="Cavalier_modification.php" method="post">
-                                <input type="hidden" name="id_personne" value="<?php echo $row["id_personne"] ?>">
-                                <input type="button" name="id_personne" class='btn btn-primary' value="Modifier" value2="<?php echo $row["id_personne"] ?>" onclick="window.location='Cavalier_modification.php';">
-                            </form>
-                            <form action="Cavalier_suppr.php" method="post">
-                                <input type="hidden" name="id" value="<?php echo $row["id_personne"] ?>">
-                                <button type="submit" name="delete" class="delete-btn btn btn-danger" onclick="return confirm('Etes vous sûre ?');">Supprimer</button>
-                            </form>
-                            </td>
-                        </tr>
-                        <?php
-                            }
-                        ?>
-                    </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
-<?php
-
- */
+            </body>
+            </html>
