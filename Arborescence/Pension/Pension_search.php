45d8e72333c2d281b33edb8472d203fe6ab3f1ec
Sawney <loris.stvl@gmail.com>
2022-11-10 17:01:46 +0100

    search réglé

--- Arborescence/Pension/Pension_search.php
+++ Arborescence/Pension/Pension_search.php
@@ -2,7 +2,7 @@
 include_once('../include/defines.inc.php');
 
 $sql = "SELECT * FROM pension
-        WHERE lib_pension = :lib_pension";
+        WHERE ref_cheval = :ref_cheval";
 $req = $conn->prepare($sql);
 $req->bindValue(':ref_cheval',$_POST['ref_cheval'],PDO::PARAM_STR);
 $res = $req->execute();
@@ -21,6 +21,12 @@ $res = $req->execute();
                         <tr>
                             <th style='text-align :center'>id_pension</th>
                             <th style='text-align :center'>lib_pension</th>
+                            <th style='text-align :center'>date_deb_pension</th>
+                            <th style='text-align :center'>duree_pension</th>
+                            <th style='text-align :center'>tarif_pension</th>
+                            <th style='text-align :center'>ref_cheval</th>
+                            <th style='text-align :center'>ref_type_p</th>
+                            <th style='text-align :center'>ref_per</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -30,6 +36,12 @@ $res = $req->execute();
                         <tr data-value="<?php echo $value["lib_pension"] ?>">
                             <td><center><?php echo $value["id_pension"] ?></center></td>
                             <td><center><?php echo $value["lib_pension"] ?></center></td>
+                            <td><center><?php echo $value["date_deb_pension"] ?></center></td>
+                            <td><center><?php echo $value["duree_pension"] ?></center></td>
+                            <td><center><?php echo $value["tarif_pension"] ?></center></td>
+                            <td><center><?php echo $value["ref_cheval"] ?></center></td>
+                            <td><center><?php echo $value["ref_type_p"] ?></center></td>
+                            <td><center><?php echo $value["ref_per"] ?></center></td>
                         </tr>
                         <?php
                             }
