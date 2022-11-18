7b82e1c084fb6e913e88626398279d3de4f85044
Raphaël D <daniel.rapahel@hotmail.fr>
2022-11-18 12:20:36 +0100

    rajout d'autre truc

---
+++ Arborescence/Cours/fullcalendar-master/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3c92317f897ec0c4428029799a8d3fcf3d9b12e1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3c92317f897ec0c4428029799a8d3fcf3d9b12e1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'dcblogdev/pdo-wrapper' => array(
+            'pretty_version' => 'v2.0.1',
+            'version' => '2.0.1.0',
+            'reference' => '6c048b1838858d3d825b75f11b98d13b6cf43eee',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../dcblogdev/pdo-wrapper',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
