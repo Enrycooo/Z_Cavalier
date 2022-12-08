f97f22b57c6dea67b5549a12aa82bf10d249a88a
Raphaël D <daniel.rapahel@hotmail.fr>
2022-12-08 14:26:25 +0100

    ajout PDO pour inscription

--- Arborescence/registration/register.php
+++ Arborescence/registration/register.php
@@ -5,24 +5,28 @@
 </head>
 <body>
 <?php
-require('config.php');
+require('../include/defines.inc.php');
 
 if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
+    
 	// récupérer le nom d'utilisateur et supprimer les antislashes ajoutés par le formulaire
 	$username = stripslashes($_REQUEST['username']);
-	$username = mysqli_real_escape_string($conn, $username); 
+        
 	// récupérer l'email et supprimer les antislashes ajoutés par le formulaire
 	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($conn, $email);
+        
 	// récupérer le mot de passe et supprimer les antislashes ajoutés par le formulaire
 	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($conn, $password);
-	
-	$query = "INSERT into `users` (username, email, type, password)
-				VALUES ('$username', '$email', 'user', '".hash('sha256', $password)."')";
-	$res = mysqli_query($conn, $query);
+        
+        $request = "INSERT into `users` (username, email, type, password)
+                    VALUES (:name, :mail, 'user', ".hash('sha256', ":pwd").")";
+        $sql = $conn->prepare($request);
+        $sql->bindValue(':name', $username, PDO::PARAM_STR);
+        $sql->bindValue(':mail', $email, PDO::PARAM_STR);
+        $sql->bindValue(':pwd', $password, PDO::PARAM_STR);
+        $sql->execute();
 
-    if($res){
+    if($sql){
        echo "<div class='sucess'>
              <h3>Vous êtes inscrit avec succès.</h3>
              <p>Cliquez ici pour vous <a href='login.php'>connecter</a></p>
